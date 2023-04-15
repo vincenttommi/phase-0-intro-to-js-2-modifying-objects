8d21e0e154e5d2499958bbb71c500d803fb0aa24
vincenttommi <vincenttommi@gmail.com>
2023-04-15 16:50:23 +0300

    deleting objects in js

---
+++ deletingobjects.js
@@ -0,0 +1,22 @@
+const wednesdayMenu = {
+
+cheesePlate:{
+
+    soft: "Brie",
+    semiSoft: "Fontina",
+    hard : "Provolone",
+
+},
+
+
+
+fries : "Sweet Potato",
+salad : "SouthWestern",
+
+};
+
+console.log(delete wednesdayMenu.salad);
+
+
+//Deleting objects in js 
+
--- mofying.js
+++ mofying.js
@@ -1,18 +1,57 @@
 
+// const mondayMenu = {
+
+
+//     cheesePlate :{
+
+
+//         soft:"chevre",
+//         semisoft :"Gruyere",
+//         hard : "Manchego",
+//     },
+
+//     fries : "Curly",
+//     salad : "Cobb",
+
+// };
+
+// console.log(mondayMenu);
+
+
+
+
+// function destructivelyUpdateObject(obj,key, value){
+
+
+//  obj[key] = value;  //why are we using bracket notation here
+
+//  return obj
+
+
+
+// }
+
+
+
 const mondayMenu = {
-
-
-    cheesePlate :{
-
-
-        soft:"chevre",
-        semisoft :"Gruyere",
-        hard : "Manchego",
+    cheesePlate: {
+      soft: "Chèvre",
+      semiSoft: "Gruyère",
+      hard: "Manchego",
     },
+    fries: "Sweet potato",
+    salad: "Cobb",
+  };
+  
+  const tuesdayMenu = destructivelyUpdateObject(mondayMenu, "salad", "Caesar");
+  
+  tuesdayMenu;
+  //=> { cheesePlate: { soft: "Chèvre", semiSoft: "Gruyère", hard: "Manchego" }, fries: "Sweet potato", salad: "Caesar" }
+  
+  tuesdayMenu.salad;
+  //=> "Caesar"
 
-    fries : "Curly",
-    salad : "Cobb",
 
-};
 
-console.log(mondayMenu);
+
+
